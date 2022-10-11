ae561f088664dd83588bfb0f7fa3a094752b587b
CirodeCristofaro <ci.decristofaro@gmail.com>
2022-10-11 15:28:14 +0200

    Ciro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContainerComponent } from './container/container.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: "login", component: LoginComponent },
+  { path: "login", component: LoginComponent }, {path: '', component: ContainerComponent}
 ];
 
 @NgModule({
